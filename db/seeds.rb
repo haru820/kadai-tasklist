5baccb5ef2775e10d676ffc18e3f716c58d8d483
haru <haru820@hotmail.com>
2020-02-09 05:53:42 +0000

    Paginate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..100).each do |num|
+  Task.create(content: 'test ' + num.to_s, status: 'not ' + num.to_s)
+end
